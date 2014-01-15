c89f5e52b143bef0c027fabd4677fc0b53fcf6fa
Jesse Wolff <jessewolff@gmail.com>
2014-01-14 19:08:03 -0800

    Initial Commit

---
+++ AddToTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AddToTableView
+//
+//  Created by Jesse Wolff on 1/14/14.
+//  Copyright (c) 2014 Soleares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AddToTableViewTests/AddToTableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  AddToTableViewTests.m
+//  AddToTableViewTests
+//
+//  Created by Jesse Wolff on 1/14/14.
+//  Copyright (c) 2014 Soleares. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AddToTableViewTests : XCTestCase
+
+@end
+
+@implementation AddToTableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
